82d1edbaf4c0786f80b3e20819d9c486fd2baa8d
Alan Lima <alima@outlook.com>
2017-10-27 23:24:51 +1000

    added a padding at fixed-table-pagination to better display and changed the default option for button css

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -343,7 +343,7 @@
         searchTimeOut: 500,
         searchText: '',
         iconSize: undefined,
-        buttonsClass: 'default',
+        buttonsClass: 'secondary',
         iconsPrefix: 'glyphicon', // glyphicon of fa (font awesome)
         icons: {
             paginationSwitchDown: 'glyphicon-collapse-down icon-chevron-down',
